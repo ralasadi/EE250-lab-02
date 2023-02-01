d1904c8554a20c07eeaeab4b78e2708974121a4d
ralasadi <ralasadi@usc.edu>
2023-01-31 17:01:59 -0800

    commit

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 [Fork](https://docs.github.com/en/get-started/quickstart/fork-a-repo) this repo and clone it to your machine to get started!
 
 ## Team Members
-- team member 1
-- team member 2
+- Riad Alasadi 
+- Jerry Appelhans
 
 ## Lab Question Answers
 
 Answer for Question 1: 
 
-...
+The reliability of UDP diminished when 50% loss was added. When trying to send a sequence, sometimes the message would get lost. This would happen because 50% loss was added to the UDP.
+
+Answer for Question 2:
+
+The reliability of TCP did not change because the server still received the messages but just at a slower rate. This is because it is resending the messages that it lost.
+
+Answer for Quesiton 3:
+
+The speed of the TCP response decreases when the 50% loss was added. This is because it is resending the loss.
+
--- tcp_client.cpp
+++ tcp_client.cpp
@@ -15,14 +15,16 @@ int main(int argc, char const *argv[])
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
 	
+	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = argv[1];
+	std::string server_port = argv[2];
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+	client_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
@@ -35,6 +37,7 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 	
+	
 	// Helping you out by pepping the struct for connecting to the server
 	struct addrinfo hints;
 	struct addrinfo *server_addr;
@@ -44,10 +47,22 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
+	
+	int connected = connect(client_fd, server_addr->ai_addr,server_addr->ai_addrlen);
+	
 	// TODO: Retreive user input
+	std:: string user_input;
+	//std::cout << "hello"<< std::endl;
+	std::getline(std::cin,user_input);
+	
+	
 	// TODO: Send() the user input to the server
+	int sent_result = send(client_fd, user_input.c_str(), sizeof(user_input), 0);
 	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
+	int received = recv(client_fd, socket_read_buffer,1024,0);
+	std::cout << socket_read_buffer << "\r\n";
 	// TODO: Close() the socket
+	close(client_fd);
 
 	return 0; 
-} 
+}
